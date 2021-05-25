1625dffeb37bfbefa5de418af99e22e241552044
Irina Nasyrova <irina.nasyrova@swagmasha.org>
2021-05-25 20:09:15 +0500

    fix

--- src/main/java/ru/skillfactory_tgbot/tgbot/controllers/CurrencyController.java
+++ src/main/java/ru/skillfactory_tgbot/tgbot/controllers/CurrencyController.java
@@ -1,7 +1,9 @@
 package ru.skillfactory_tgbot.tgbot.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skillfactory_tgbot.tgbot.dto.ValuteCursOnDate;
 import ru.skillfactory_tgbot.tgbot.service.CentralRussianBankService;
@@ -15,7 +17,14 @@ public class CurrencyController {
     private final CentralRussianBankService centralRussianBankService;
 
     @GetMapping("/getCurrencies")
+    @ApiOperation(value = "Получение курса всех валют на текущий день")
     public List<ValuteCursOnDate> getValuteCursOnDate() throws Exception {
         return centralRussianBankService.getCurrenciesFromCbr();
+    }
+
+    @GetMapping("/getCurrency/{code}")
+    @ApiOperation(value = "Получение курса определенно валюты на текущий день")
+    public ValuteCursOnDate getCourseForCurrency(@PathVariable String code) throws Exception {
+        return centralRussianBankService.getCourseForCurrency(code);
     }
 }
--- src/main/java/ru/skillfactory_tgbot/tgbot/service/CentralRussianBankService.java
+++ src/main/java/ru/skillfactory_tgbot/tgbot/service/CentralRussianBankService.java
@@ -18,7 +18,6 @@ public class CentralRussianBankService extends WebServiceTemplate {
     @Value("${cbr.api.url}")
     private String cbrApiUrl;
 
-    //Создаем метод получения данных
     public List<ValuteCursOnDate> getCurrenciesFromCbr() throws DatatypeConfigurationException {
         final GetCursOnDateXml getCursOnDateXML = new GetCursOnDateXml();
         GregorianCalendar cal = new GregorianCalendar();
@@ -27,14 +26,19 @@ public class CentralRussianBankService extends WebServiceTemplate {
         XMLGregorianCalendar xmlGregCal = DatatypeFactory.newInstance().newXMLGregorianCalendar(cal);
         getCursOnDateXML.setOnDate(xmlGregCal);
 
+        System.out.println(getCursOnDateXML);
         GetCursOnDateXmlResponse response = (GetCursOnDateXmlResponse) marshalSendAndReceive(cbrApiUrl, getCursOnDateXML);
 
         if (response == null) {
-            throw new IllegalStateException("Could not get response from CBR Service");
+            throw new IllegalStateException("Не удалось получить данные от ЦБ РФ");
         }
 
         final List<ValuteCursOnDate> courses = response.getGetCursOnDateXmlResult().getValuteData();
         courses.forEach(course -> course.setName(course.getName().trim()));
         return courses;
+    }
+
+    public ValuteCursOnDate getCourseForCurrency(String code) throws DatatypeConfigurationException {
+        return getCurrenciesFromCbr().stream().filter(currency -> code.equals(currency.getChCode())).findFirst().get();
     }
 }
